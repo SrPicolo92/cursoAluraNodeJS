8e8126ca82ab0d67eabeefc800647eaec2f3c6d6
SrPicolo92 <45546949+SrPicolo92@users.noreply.github.com>
2021-06-18 16:29:08 -0300

    Update conexao.js
    tirando a senha

--- infraestrutura/database/conexao.js
+++ infraestrutura/database/conexao.js
@@ -3,7 +3,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3307,
     user: 'root',
-    password: 'FeRa27.23',
+    password: '********',
     database: 'agenda-petshop'
 })
 
